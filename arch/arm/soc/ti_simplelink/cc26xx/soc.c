fa658e17a84c034aa3d27fe62c60272f4a0c4af0
Xue Liu <xue.liu@dks-koeln.de>
2017-04-10 08:12:35 +0200

    init
    Signed-off-by: Xue Liu <xue.liu@dks-koeln.de>

---
+++ arch/arm/soc/ti_simplelink/cc26xx/soc.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016, Texas Instruments Incorporated
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <soc.h>
+#include <driverlib/rom.h>
+#include <driverlib/rom_map.h>
+#include <driverlib/prcm.h>
+
+static int ti_cc2650_init(struct device *arg)
+{
+	ARG_UNUSED(arg);
+
+//	PRCMCC3200MCUInit();
+
+#ifdef CONFIG_UART_CC2650
+	/*
+	 * Enable Peripheral Clocks, ensuring UART can wake the processor from
+	 * idle (after ARM wfi instruction)
+	 */
+//	MAP_PRCMPeripheralClkEnable(PRCM_UARTA0, PRCM_RUN_MODE_CLK |
+//				    PRCM_SLP_MODE_CLK);
+#endif
+
+	return 0;
+}
+
+SYS_INIT(ti_cc2650_init, PRE_KERNEL_1, 0);
